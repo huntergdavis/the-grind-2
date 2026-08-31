441e8bbb4ca6f68a076b0dd0bcda26427bbf97c2
hunter@hunterdavis.com <hunter@hunterdavis.com>
2026-08-31 11:24:57 -0700

    chore: release v0.5.36

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "the-grind-2:assets:v0.5.35";
+const cacheName = "the-grind-2:assets:v0.5.36";
 const shell = ["./", "./index.html"];
 
 self.addEventListener("install", (event) => {
